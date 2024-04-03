f9622245a69ec16b4bda820630d0bff04a4209cf
Guilherme Alvarez <guilherme.zalv@gmail.com>
2024-04-03 19:23:38 -0300

    Primeiro Passo Completado

---
+++ src/05_challenge.sql
@@ -0,0 +1,9 @@
+SELECT
+    full_name AS 'Nome completo',
+    birthday AS 'Data de nascimento'
+FROM
+    users
+WHERE
+    active = 1
+    AND
+    YEAR(birthday) >= 1990;
---
+++ src/08_challenge.sql
@@ -0,0 +1,10 @@
+SELECT
+    full_name AS "Nome completo",
+    email AS "E-mail",
+    p.name AS "Plano"
+FROM
+    users u
+JOIN
+    plans p ON u.plan_id = p.id
+ORDER BY
+    full_name;
